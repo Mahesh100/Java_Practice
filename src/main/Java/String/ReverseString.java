15dbcf0776e08ef69f8ad9753138561d113c4162
Mahesh Kakde <maheshkakde100@gmail.com>
2024-03-01 10:37:24 +0530

    Reverse a String in java

---
+++ src/main/Java/String/ReverseString.java
@@ -0,0 +1,27 @@
+package String;
+
+public class ReverseString {
+
+    public static void main(String [] args) {
+
+        //First way is to use con-carnation operation in java.
+
+        String s = "ABCD";
+        String revers = null;
+
+        s.length();
+
+        System.out.println(s.length());
+
+        for(int i = 0; i<s.length(); i++){
+            System.out.print( s.charAt(i));
+
+        }
+
+
+    /*
+    -We need to find out length of string
+     */
+    }
+
+}
