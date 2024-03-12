99cb773bfa05f845f0f89833f1aa6256df3966eb
Mahesh Kakde <maheshkakde100@gmail.com>
2024-03-13 00:09:56 +0530

    updated

--- src/main/Java/String/ReverseString.java
+++ src/main/Java/String/ReverseString.java
@@ -6,10 +6,9 @@ public class ReverseString {
 
         String str = "ABCD";
 
-
         System.out.println(str.length());
 
-        for(int i=str.length()-1; i>=0; i--){        //str.lenght() -1 is taken to find the index of lost character
+        for(int i=str.length()-1; i>=0; i--){        //str.length() -1 is taken to find the index of lost character
             System.out.print(str.charAt(i));
         }
 
