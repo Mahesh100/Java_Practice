742be91b6a22d502ff50992634748afd94ebe9bb
Mahesh Kakde <maheshkakde100@gmail.com>
2024-03-25 19:01:55 +0530

    updated

---
+++ src/main/Java/Deepak_tutorial/Encapsulation/A.java
@@ -0,0 +1,14 @@
+package Deepak_tutorial.Encapsulation;
+
+public class A {
+
+    private int value;
+
+    public void setValue(int x){
+            value =x;
+    }
+
+    public int getValue(){
+            return +value; //Increment
+    }
+}
---
+++ src/main/Java/Deepak_tutorial/Encapsulation/B.java
@@ -0,0 +1,11 @@
+package Deepak_tutorial.Encapsulation;
+
+public class B {
+
+    public static void main(String [] args){
+        A obj = new A();
+        obj.setValue(100);
+        System.out.println("Value is :"+obj.getValue());
+
+    }
+}
