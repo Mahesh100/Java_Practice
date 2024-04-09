3b408120b7dfb8beed329e132781915844f51f70
Mahesh Kakde <maheshkakde100@gmail.com>
2024-04-09 23:21:17 +0530

    added

--- src/main/Java/LambadaExpression/PredefineLambada/Test.java
+++ src/main/Java/LambadaExpression/PredefineLambada/Test.java
@@ -5,10 +5,13 @@ interface Predicate<T>
 {
     public boolean test(T t);
 }
+
 public class Test {
 
     public static void main(String[] args) {
 
         Predicate<Integer> p = i ->(i>10);
+        boolean result = p.test(2);
+        System.out.println(result);
     }
 }
