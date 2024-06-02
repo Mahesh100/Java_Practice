2b55d972bb47a1040d3d88f598a385c9832c231b
Mahesh Kakde <maheshkakde100@gmail.com>
2024-06-02 23:39:34 +0530

    added

--- src/main/Java/Practice/Programs/ReverseString.java
+++ src/main/Java/Practice/Programs/ReverseString.java
@@ -1,18 +1,21 @@
 package Practice.Programs;
 
+
+
+
 public class ReverseString {
-
-    public static void main(String[] args) {
-
+//Reverse string in java
+    public static void main(String[] args){
 
         String str = "Mahesh";
 
-        int strLength = str.length();
+        int length = str.length();
         String reverse ="";
-        for (int i = strLength - 1; i >= 0; i--) {
-            reverse = reverse + str.charAt(i);
+        for(int i=length-1; i>=0; i--){
+            reverse = reverse+str.charAt(i);
         }
 
         System.out.println(reverse);
+
     }
 }
