47c8876f8c12d5c382e0491e63be184f70deceb5
Mahesh Kakde <maheshkakde100@gmail.com>
2024-06-07 18:52:11 +0530

    Methods and constructors in multithreading

---
+++ src/main/Java/Threads/MultiThreading/ThreadCases/Case3/Test.java
@@ -0,0 +1,41 @@
+package Threads.MultiThreading.ThreadCases.Case3;
+
+
+//Performing multiple task from multiple threads
+class MyThread1 extends Thread{
+    public void run()
+    {
+        System.out.println(Thread.currentThread().getName()+ " Task1");
+    }
+}
+
+class  MyThread2 extends Thread{
+    public void run(){
+        System.out.println(Thread.currentThread().getName()+ " Task2");
+    }
+}
+
+class MyThread3 extends Thread{
+    public void run(){
+        System.out.println(Thread.currentThread().getName()+ " Task3");
+    }
+}
+
+public class Test {
+
+    public static void main(String[] args) {
+
+
+        MyThread1 t1 = new MyThread1();
+        t1.start();
+
+        MyThread2 t2 = new MyThread2();
+        t2.start();
+
+        MyThread3 t3 = new MyThread3();
+        t3.start();
+    }
+
+}
+
+
