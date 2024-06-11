06f0cf567991ea2c8e84b5b085b19f1afbf78e37
Mahesh Kakde <maheshkakde100@gmail.com>
2024-06-11 11:56:22 +0530

    Static Synchronization

--- src/main/Java/Threads/MultiThreading/Synchronization/BookTheaterSeat.java
+++ src/main/Java/Threads/MultiThreading/Synchronization/BookTheaterSeat.java
@@ -1,17 +1,22 @@
 package Threads.MultiThreading.Synchronization;
 
 public class BookTheaterSeat {
-    int total_seats = 10;
-    void bookSeat(int seats)
+    int total_seats = 15;
+ //  synchronized  void bookSeat(int seats)
+   void bookSeat(int seats)
     {
+        synchronized (this){
+
         if(total_seats>=seats)
         {
             System.out.println("Seats booked successfully");
             total_seats =total_seats-seats;
             System.out.println("Left seats: "+total_seats);
         }else {
-            System.out.println("Seats can not be booked");
-            System.out.println("Seats Left "+total_seats);
+            System.out.println("Seats can not be booked as seats left only : "+total_seats);
+
         }
+        }
+
     }
 }
---
+++ src/main/Java/Threads/MultiThreading/Synchronization/StaticSynchronization/MovieBookApp.java
@@ -0,0 +1,68 @@
+package Threads.MultiThreading.Synchronization.StaticSynchronization;
+
+import Threads.MultiThreading.Synchronization.StaticSynchronization.BookTheaterSeat;
+
+
+public class MovieBookApp {
+
+    public static void main(String[] args) {
+        BookTheaterSeat b1 = new BookTheaterSeat();
+
+        MyThread1 t1 = new MyThread1(b1,7);
+        t1.start();
+
+        MyThread2 t2 = new MyThread2(b1, 8);
+        t2.start();
+
+        BookTheaterSeat b2 = new BookTheaterSeat();
+        MyThread3 t3 = new MyThread3(b1, 9);
+        t3.start();
+    }
+
+}
+
+class MyThread1 extends Thread{
+      BookTheaterSeat b;
+    int seats;
+    MyThread1(BookTheaterSeat b, int seats)
+    {
+        this.b=b;
+        this.seats=seats;
+
+    }
+
+    public void run(){
+        b.bookSeat(seats);
+    }
+}
+
+class MyThread2 extends  Thread{
+    BookTheaterSeat b;
+
+    int seats;
+
+    MyThread2(BookTheaterSeat b, int seats){
+        this.b =b;
+        this.seats =seats;
+    }
+
+    public void run(){
+        b.bookSeat(seats);
+    }
+}
+
+
+class MyThread3 extends  Thread{
+    BookTheaterSeat b;
+
+    int seats;
+
+    MyThread3(BookTheaterSeat b, int seats){
+        this.b =b;
+        this.seats =seats;
+    }
+
+    public void run(){
+        b.bookSeat(seats);
+    }
+}
