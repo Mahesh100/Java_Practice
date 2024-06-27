b1f53493a7766eafb8ed3c308685cac7b2d8547e
Mahesh Kakde <maheshkakde100@gmail.com>
2024-06-27 12:29:02 +0530

    Notes updated

--- src/main/Java/SuperKeyword/A.java
+++ src/main/Java/SuperKeyword/A.java
@@ -3,11 +3,20 @@ package SuperKeyword;
 public class A {
 
     int a =10;
+
+    void  display(){
+        System.out.println("Display method");
+    }
+
 }
 
 class B extends A
 {
     int a = 20;
+
+    public  B(){
+        super.display();
+    }
     void show(){
         System.out.println(a);
         System.out.println(super.a);
@@ -16,6 +25,7 @@ class B extends A
     public static void main(String[] args) {
         B r = new B();
         r.show();
+
 
     }
 }
