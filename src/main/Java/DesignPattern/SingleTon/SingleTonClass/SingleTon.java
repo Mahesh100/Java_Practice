69fc84cb28f2f40afe921eebe20a17ab0f8c7ce2
Mahesh Kakde <maheshkakde100@gmail.com>
2024-06-28 15:20:40 +0530

    Singleton class in java

---
+++ src/main/Java/DesignPattern/SingleTon/SingleTonClass/SingleTon.java
@@ -0,0 +1,25 @@
+package DesignPattern.SingleTon.SingleTonClass;
+
+public class SingleTon
+{
+
+    public static void main(String[] args)
+    {
+
+        Abc obj1 = Abc.getInstanceOfAbc();
+    }
+}
+
+class Abc{
+
+    static Abc obj = new Abc();
+
+    private Abc(){
+
+    }
+
+    public static Abc getInstanceOfAbc(){
+        return obj;
+    }
+
+}
