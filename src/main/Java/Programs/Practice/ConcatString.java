5fad716514c43ba83410c8483fc2c42893f382f5
Mahesh Kakde <maheshkakde100@gmail.com>
2024-07-29 11:26:40 +0530

    added

---
+++ src/main/Java/Programs/Practice/ConcatString.java
@@ -0,0 +1,11 @@
+package Programs.Practice;
+
+public class ConcatString {
+
+    public static void main(String [] args){
+        String x = "abc";
+        String y= "abc";
+        x.concat(y);
+        System.out.println(x);
+    }
+}
