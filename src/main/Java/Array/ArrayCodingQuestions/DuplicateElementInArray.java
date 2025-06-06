a6b4258d52d4abdca15e1968838415b8e594344a
Mahesh Kakde <maheshkakde100@gmail.com>
2025-06-06 22:02:44 +0530

    find duplicate element in an array

---
+++ src/main/Java/Array/ArrayCodingQuestions/DuplicateElementInArray.java
@@ -0,0 +1,18 @@
+
+package Array.ArrayCodingQuestions;
+
+public class DuplicateElementInArray {
+
+    public static void main(String [] args){
+        int[] inputArray = {1,2,3,4,5,5,6,6};
+
+        for(int i=0; i<inputArray.length; i++){
+            for(int j=i+1; j<inputArray.length; j++){
+                if(inputArray[i] == inputArray[j]){
+                    System.out.println("Duplicate element is :"+inputArray[i]);
+                }
+            }
+        }
+    }
+
+}
