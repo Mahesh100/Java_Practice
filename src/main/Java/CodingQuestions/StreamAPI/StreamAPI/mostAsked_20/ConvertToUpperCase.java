791f5e053319f130b2a315f7081385a279bc3152
Mahesh <maheshkakde100@gmail.com>
2025-07-03 15:56:54 +0530

    stream api questions added

---
+++ src/main/Java/CodingQuestions/StreamAPI/StreamAPI/mostAsked_20/ConvertToUpperCase.java
@@ -0,0 +1,23 @@
+package CodingQuestions.StreamAPI.StreamAPI.mostAsked_20;
+
+//Convert the list of String to upper case
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+public class ConvertToUpperCase {
+    public static void main(String [] args){
+        List<String> stringList = Arrays.asList("banana","mango","apple");
+
+        List<String> resultList = stringList.stream().map(n->n.toUpperCase()).collect(Collectors.toList());
+        System.out.println(resultList);
+
+        // Second  approach
+
+        List<String> resultList2 = stringList.stream().map(String::toUpperCase).collect(Collectors.toList());
+        System.out.println(resultList2);
+
+    }
+}
