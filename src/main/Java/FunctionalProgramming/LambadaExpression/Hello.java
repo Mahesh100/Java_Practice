a0c3890dd1580feb73c728aa72ba7f4438e6a4c8
Mahesh <maheshkakde100@gmail.com>
2025-07-03 09:37:09 +0530

    lambada expression notes updated

---
+++ src/main/Java/FunctionalProgramming/LambadaExpression/Hello.java
@@ -0,0 +1,20 @@
+package FunctionalProgramming.LambadaExpression;
+
+public class Hello {
+//    private void sayHello(){
+//        System.out.println("Hello");
+//    }
+
+    public static void add(int a, int b){
+        System.out.println(a+b);
+    }
+
+    public static void main(String [] args){
+//        () -> { System.out.println("Hello");}
+        int a= 10;
+        int b=20;
+        add(a,b);
+    }
+
+
+}
---
+++ src/main/Java/Programs/StreamAPI/FindMaxNumber.java
@@ -0,0 +1,14 @@
+package Programs.StreamAPI;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Arrays;
+
+public class FindMaxNumber {
+    public static  void  main(String [] args){
+        List<Integer> list = Arrays.asList(1,2,3,4);
+
+        int max = list.stream().max(Comparator.comparing(Integer::valueOf)).get();
+        System.out.println(max);
+    }
+}
