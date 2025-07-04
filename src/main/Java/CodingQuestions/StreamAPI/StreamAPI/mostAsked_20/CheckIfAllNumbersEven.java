825be61ab432bbdc382860a18268a7fb1773bdfc
Mahesh <maheshkakde100@gmail.com>
2025-07-04 10:52:11 +0530

    stream api questions added

---
+++ src/main/Java/CodingQuestions/StreamAPI/StreamAPI/mostAsked_20/CheckIfAllNumbersEven.java
@@ -0,0 +1,14 @@
+package CodingQuestions.StreamAPI.StreamAPI.mostAsked_20;
+//Check if all numbers are even
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CheckIfAllNumbersEven {
+    public static void main(String [] args){
+        List<Integer> numberlist = Arrays.asList(1,2,4,5);
+
+        boolean check = numberlist.stream().allMatch(n->n%2==0);
+        System.out.println(check);
+    }
+}
