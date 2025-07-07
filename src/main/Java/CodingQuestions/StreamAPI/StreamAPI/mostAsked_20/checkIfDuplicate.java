bbb8d97d926f9793385dee3d6c0cba36f27ef7c5
Mahesh <maheshkakde100@gmail.com>
2025-07-07 11:39:39 +0530

    java notes updated

---
+++ src/main/Java/CodingQuestions/StreamAPI/StreamAPI/mostAsked_20/checkIfDuplicate.java
@@ -0,0 +1,17 @@
+package CodingQuestions.StreamAPI.StreamAPI.mostAsked_20;
+
+import java.util.HashSet;
+
+public class checkIfDuplicate {
+    public static void main(String [] args){
+        HashSet s = new HashSet();
+        s.add(1);
+        s.add(2);
+        s.add(2);
+
+        System.out.println(s.size());
+
+        String str = "   Hello Word                          ";
+        System.out.println(str.strip());
+    }
+}
