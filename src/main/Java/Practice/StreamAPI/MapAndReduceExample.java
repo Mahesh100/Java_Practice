ec3ef821cbd3e11165a05c19d6c5113472afb25c
Renka1818 <renukakakde23@gmail.com>
2025-07-16 12:21:17 +0530

    stream api notes updated

--- src/main/Java/Practice/StreamAPI/MapAndReduceExample.java
+++ src/main/Java/Practice/StreamAPI/MapAndReduceExample.java
@@ -4,22 +4,31 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+//Get sum of all the elements in list
+
 public class MapAndReduceExample {
     public static void main(String [] args){
         List<Integer> numbers = Arrays.asList(3,7,8,1,5,9);
 
+        //Using mapToInt
         int sum1 =  numbers.stream().mapToInt(i->i).sum();
+        System.out.println(sum1);
 
+        //Using reduce() method & lambada
         Integer reduceSum = numbers.stream().reduce(0,(a,b)->a+b);
         System.out.println(reduceSum);
 
+        //Using reduce method & method reference
         Optional<Integer> reduceSum2 = numbers.stream().reduce(Integer::sum);
         System.out.println(reduceSum2.get());
 
         //Perform the multiplication of numbers
-
         Integer productOfNumbers = numbers.stream().reduce(1,(a,b)->a*b);
         System.out.println(productOfNumbers);
+
+        //checking if sorted() have mapToInt() method
+
+        int sum3 = numbers.stream().map()
 
 //        int sum=0;
 //        for(int no: numbers){
