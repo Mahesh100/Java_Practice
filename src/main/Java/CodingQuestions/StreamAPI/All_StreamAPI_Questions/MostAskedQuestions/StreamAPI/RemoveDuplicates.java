bfe5413e503945e0e13be5cac0e665ddbdfc0b47
Mahesh100 <maheshkakde100@gmail.com>
2025-08-17 15:40:49 +0530

    remove duplicated element added

---
+++ src/main/Java/CodingQuestions/StreamAPI/All_StreamAPI_Questions/MostAskedQuestions/StreamAPI/RemoveDuplicates.java
@@ -0,0 +1,25 @@
+package CodingQuestions.StreamAPI.All_StreamAPI_Questions.MostAskedQuestions.StreamAPI;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RemoveDuplicates {
+
+    public static void main(String [] args){
+
+        List<Integer> list = Arrays.asList(1,2,3,4,4);
+
+        List<Integer> withoutDuplicates = list.stream().distinct().collect(Collectors.toList());
+        System.out.println(withoutDuplicates);
+
+        //Without using distinct method
+        HashSet<Integer> seen = new HashSet<>();
+
+        List<Integer> withoutDuplicates2 = list.stream().filter(n->seen.add(n)).collect(Collectors.toList());
+        System.out.println(withoutDuplicates2);
+
+        withoutDuplicates2.forEach(System.out::println);
+    }
+}
